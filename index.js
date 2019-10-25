b0d1d984a1044bec1f351e474a15d6daa4d483c2
SummeryTime <SummeryTime@github.com>
2019-10-26 05:22:37 +0800

    First commit

---
+++ index.js
@@ -0,0 +1,4 @@
+///  <reference no-default-lib="true"/>
+/// <reference path="./node_modules/typescript/lib/lib.d.ts" />
+/// <reference path="./node_modules/typescript/lib/lib.dom.d.ts" />
+/// <reference path="./node_modules/@types/node/ts3.2/index.d.ts" /
